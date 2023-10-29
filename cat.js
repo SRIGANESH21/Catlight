85d040f7928826c7645db98f6d91ce5ab2ee0f30
Sri Ganesh Kumar <143590674+SRIGANESH21@users.noreply.github.com>
2023-10-29 12:56:39 +0530

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,14 @@
+function switchon()
+{
+document.getElementById("imagelight").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+document.getElementById("light").innerHTML="ON";
+document.getElementById("imagecat").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png" ;
+
+}
+function switchoff()
+{
+document.getElementById("imagelight").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+document.getElementById("light").innerHTML="OFF";
+
+document.getElementById("imagecat").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+}
